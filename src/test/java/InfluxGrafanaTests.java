efba91e77933f7271fbb85ee577df61bc8c828c8
adiyar <adi.yarimi@gmail.com>
2022-07-31 21:14:38 +0300

    Initial commit

---
+++ src/test/java/InfluxGrafanaTests.java
@@ -0,0 +1,25 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class InfluxGrafanaTests {
+    @Test(description = "Login")
+    public void launchApp() throws Exception {
+        Thread.sleep(10000);
+        Assert.fail();
+    }
+    @Test(description = "Search")
+    public void sort() throws Exception {
+        Thread.sleep(10000);
+        Assert.fail();
+    }
+    @Test(description = "Select")
+    public void selectAProduct() throws Exception {
+        Thread.sleep(10000);
+
+    }
+    @Test(description = "Log Out")
+    public void goToAboutPage() throws Exception {
+        Thread.sleep(10000);
+
+    }
+}
